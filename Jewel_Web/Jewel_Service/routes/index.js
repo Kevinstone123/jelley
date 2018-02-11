6d789adca626da0652f4320187e0ee286dd29943
夏佳骏 <xiajiajun@xiajiajundeMacBook-Pro.local>
2018-02-11 15:09:40 +0800

    导入ejs

--- Jewel_Web/Jewel_Service/routes/index.js
+++ Jewel_Web/Jewel_Service/routes/index.js
@@ -1,19 +1,42 @@
 const router = require('koa-router')()
+const CommonJs = require("../library/common.js");
+const fs = require('fs')
+const path = require('path')
+const mobileUrl = path.resolve(__dirname, '..');   //获取上一次的目录
 
+// PC版首页
 router.get('/', async (ctx, next) => {
-  await ctx.render('index', {
-    title: 'Hello Koa 2!'
-  })
-})
 
-router.get('/string', async (ctx, next) => {
-  ctx.body = 'koa2 string'
-})
-
-router.get('/json', async (ctx, next) => {
-  ctx.body = {
-    title: 'koa2 json'
+  /**
+   * 通用方法-指派重定向
+  */
+  let url = CommonJs.isMobile(ctx,'Pc');
+  if (url !== '') {
+    ctx.redirect(url);
   }
+  /*** END ***/
+
+  let model = {
+    "title" : "首页"
+  };
+
+  await ctx.render('pc/home/home', model )
+})
+
+// Mobile首页-所有
+router.get('/mobile/*',async(ctx, next) => {
+
+  /**
+   * 通用方法-指派重定向
+  */
+  let url = CommonJs.isMobile(ctx,'Mobile');
+  if (url !== '') {
+    ctx.redirect(url);
+  }
+  /*** END ***/
+
+  ctx.type = 'html';
+  ctx.body = fs.createReadStream(mobileUrl + '/public/mobile/dist/index.html');
 })
 
 module.exports = router
